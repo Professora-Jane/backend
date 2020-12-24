d8f66b216a809926c70f9ed4ff7ee23ba9b9cb4c
Elias Bernardo <ebmm01@gmail.com>
2020-12-24 07:59:11 -0300

    Adding db singleton config

---
+++ src/db/index.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+class Db {
+    constructor() {
+        this._mongoose = undefined
+    }
+
+    async connect({ connectionString, options }) {
+        if (this._mongoose == undefined)
+            this._mongoose = await mongoose.connect(connectionString, options);
+    }
+
+    async getCollection(modelName, schema) {
+        if (this._mongoose == undefined)
+            throw new Error("Conexão com o banco não realizada!!")
+
+        return this._mongoose.model(modelName, schema);
+    }
+}
+
+
+const instance = new Db();
+
+module.exports = {
+    dbInstance: () => instance
+}
