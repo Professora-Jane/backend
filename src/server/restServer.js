eadb049157a3c3dc6875437dc8d9eaadfd53d888
Elias Bernardo <ebmm01@gmail.com>
2020-12-26 00:39:12 -0300

    Renaming server/index.js to server/restServer.js

---
+++ src/server/restServer.js
@@ -0,0 +1,48 @@
+const fastify = require("fastify");
+const { FastifyInstance } = require("fastify");
+const fs = require("fs");
+
+class RestServer {
+
+
+    constructor() {
+        /**
+         * @type { FastifyInstance }
+         * @public
+         */
+        this.server = undefined
+    }
+
+    get log() {
+        return this.server.log;
+    }
+
+    registerRoutes({ routesPath, prefix }) {
+        fs.readdirSync(routesPath).forEach(file => {
+            this.server.register(require(routesPath + file), { prefix })
+        });
+        
+        return this;
+    }
+
+    registerErrorHandler(handler) {
+        this.server.setErrorHandler(handler)
+        return this;
+    }
+
+    configureServer(serverOpts) {
+        this.server = fastify(serverOpts);
+        return this;
+    }
+
+    async initServer(apiOpts) {
+        await this.server.listen(apiOpts.port);
+        return this;
+    }
+}
+
+const instance = new RestServer();
+
+module.exports = {
+    restServerInstance: (()=> instance)()
+}
