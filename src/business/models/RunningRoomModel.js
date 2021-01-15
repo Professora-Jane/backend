e63142e7be590074bfbc44e9eba589b72abec117
Elias Bernardo <ebmm01@gmail.com>
2021-01-15 00:06:50 -0300

    Adding RunningRoomModel

---
+++ src/business/models/RunningRoomModel.js
@@ -0,0 +1,27 @@
+const Nohm = require('nohm').Nohm;
+const NohmModel = require('nohm').NohmModel;
+
+class CurrentRoomModel extends NohmModel {
+
+}
+CurrentRoomModel.modelName = "CurrentRoom"
+CurrentRoomModel.definitions = {
+    roomId: {
+        type: "string",
+        index: true
+    },
+    currentParticipants: {
+        type: "json"
+    },
+    banned: {
+        type: "json"
+    },
+    admin: {
+        type: "string"
+    },
+    startTime: {
+        type: "string"
+    }
+}
+
+module.exports = Nohm.register(CurrentRoomModel)
