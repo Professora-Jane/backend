1bc79131cb694cd7dead5518669503d242473ec6
Elias Bernardo <ebmm01@gmail.com>
2021-01-16 19:11:26 -0300

    Exposing onReady

--- src/server/restServer.js
+++ src/server/restServer.js
@@ -11,6 +11,8 @@ class RestServer {
          * @public
          */
         this.server = undefined
+
+        this.onServerReady = undefined
     }
 
     get log() {
@@ -33,20 +35,39 @@ class RestServer {
     configureServer(serverOpts) {
         this.server = fastify(serverOpts);
         this.server.register(require("fastify-cors"));
-        this.server.register(require('fastify-auth'));
 
         return this;
     }
 
     configureSwagger(swaggerOpts) {
         this.server.register(require('fastify-oas'), swaggerOpts)
-
         return this
     }
 
+    registerPlugin(plugin, opts) {
+        this.server.register(plugin, opts)
+
+        return this
+    }
+    
+
     async initServer(apiOpts) {
         await this.server.listen(apiOpts.port);
+
+        this.server.ready(err => {
+            if (err) throw err;
+
+            this.onServerReady && this.onServerReady(this.server)
+        })
+
         return this;
+    }
+
+
+    registerOnServerReadyHandler(cb) {
+        this.onServerReady = cb
+
+        return this
     }
 }
 
