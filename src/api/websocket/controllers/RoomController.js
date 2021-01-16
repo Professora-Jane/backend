41566f300a05ee88467785fbe44cc01135be08e3
Elias Bernardo <ebmm01@gmail.com>
2021-01-16 19:10:05 -0300

    FIxing roomManager import and adding ROOM_PEER_OFFER]

--- src/api/websocket/controllers/RoomController.js
+++ src/api/websocket/controllers/RoomController.js
@@ -1,20 +1,22 @@
-const { roomsManagerInstance } = require("../../../business/roomManager/RoomsManager")
+const RoomManagetService = require("../../../business/services/RoomManagerService")
 const BaseWsController = require("./BaseController")
 
 const ROOM_JOIN = "on_room:join"
 const ROOM_LEAVE = "on_room:leave"
 const ROOM_CHAT = "on_room:chat"
+const ROOM_PEER_OFFER = "on_room:peerOffer"
 const ROOM_PARTICIPANT_JOIN = "room:participantJoin"
 const ROOM_PARTICIPANT_LEAVE = "room:participantLeave"
 
 class RoomController  extends BaseWsController {
     constructor() {
         super()
+        this.roomManagerService = new RoomManagetService()
     }
 
     async [ROOM_JOIN]({ roomId, participantId, participantName }) {
-        if(await roomsManagerInstance.addParticipant({ roomId, participantId, participantName }))    
-        await roomsManagerInstance.broadcastMessageToRoom({
+        if(await this.roomManagerService.addParticipant({ roomId, participantId, participantName }))    
+        await this.roomManagerService.broadcastMessageToRoom({
             roomId,
             type: ROOM_PARTICIPANT_JOIN,
             content: {
@@ -22,15 +24,15 @@ class RoomController  extends BaseWsController {
                     name: participantName,
                     id: participantId
                 },
-                currentUsers: roomsManagerInstance.getRoomDetails({ roomId }).currentParticipants
+                currentUsers: this.roomManagerService.getRoomDetails({ roomId }).currentParticipants
             }
         })
     }
     
     async [ROOM_LEAVE]({ roomId, participantId }) {
-        const removedParticipant = await roomsManagerInstance.removeParticipant({ roomId, participantId })
+        const removedParticipant = await this.roomManagerService.removeParticipant({ roomId, participantId })
         if(removedParticipant)    
-            await roomsManagerInstance.broadcastMessageToRoom({
+            await this.roomManagerService.broadcastMessageToRoom({
                 roomId,
                 type: ROOM_PARTICIPANT_LEAVE,
                 content: {
@@ -38,14 +40,22 @@ class RoomController  extends BaseWsController {
                         name: removedParticipant[0].name,
                         id: removedParticipant[0].id
                     },
-                    currentUsers: roomsManagerInstance.getRoomDetails({ roomId }).currentParticipants
+                    currentUsers: this.roomManagerService.getRoomDetails({ roomId }).currentParticipants
                 }
             })
     }
     
+    async [ROOM_PEER_OFFER]({ offer, participantId, roomId }) {
+        await this.roomManagerService.broadcastMessageToRoom({
+            roomId,
+            type: ROOM_PEER_OFFER.replace("on_", ""),
+            content: { offer, participantId }
+        })
+    }
+    
     async [ROOM_CHAT]({ roomId, participantId, participantName, messageContent }) {
 
-        await roomsManagerInstance.broadcastMessageToRoom({
+        await this.roomManagerService.broadcastMessageToRoom({
             roomId,
             type: ROOM_CHAT.replace("on_", ""),
             content: {
