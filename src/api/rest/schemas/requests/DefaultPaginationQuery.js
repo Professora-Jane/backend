1fdb4333fe4bc26fbd4eeca33b042386ddba6ef3
Elias Bernardo <ebmm01@gmail.com>
2021-01-21 13:00:43 -0300

    Adding sort and type to default pagination query request

--- src/api/rest/schemas/requests/DefaultPaginationQuery.js
+++ src/api/rest/schemas/requests/DefaultPaginationQuery.js
@@ -1,5 +1,10 @@
 module.exports = {
     page: { type: 'number' },
     limit: { type: 'number' },
-    search: { type: 'string' }
+    search: { type: 'string' },
+    sort: { type: 'string' },
+    sortType: { 
+        type: 'number',
+        enum: [-1, 1]
+    },
 }
