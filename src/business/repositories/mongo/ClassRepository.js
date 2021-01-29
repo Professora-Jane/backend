838c0e8b21aa9f0898af6104d9333850e4049ca1
Elias Bernardo <ebmm01@gmail.com>
2021-01-29 19:16:46 -0300

    Moving listClasses to ClassRepository

--- src/business/repositories/mongo/ClassRepository.js
+++ src/business/repositories/mongo/ClassRepository.js
@@ -1,10 +1,99 @@
 const ClassModel = require("../../models/ClassModel");
 const BaseRepository = require("./BaseRepository");
-const mongoose = require("mongoose");
+const { Types } = require("mongoose");
 
 class ClassRepository extends BaseRepository {
     constructor() {
         super(ClassModel)
+    }
+
+    /**
+     * 
+     * @param { object } ids - Objeto de ids
+     * @returns { Promise<Array<ClassList>> }
+     */
+    async getClasses({ ids, matchIdKey, restrictionIdKey = undefined }) {
+
+        for (let id in ids) {
+            if (typeof id === "string")
+            ids[id] = Types.ObjectId(ids[id])
+        }
+
+        const reverseKey = matchIdKey === "teacherId"? "student" : "teacher"
+
+        const response = await this.$listAggregate([
+            {
+                '$match': {
+                    [matchIdKey]: ids[matchIdKey]
+                }
+            }, {
+                '$unwind': {
+                    'path': '$daysOfWeek', 
+                    'preserveNullAndEmptyArrays': false
+                }
+            }, {
+                '$lookup': {
+                    'from': 'teacher', 
+                    'localField': 'teacherId', 
+                    'foreignField': '_id', 
+                    'as': 'teacher'
+                }
+            }, {
+                '$lookup': {
+                    'from': 'student', 
+                    'localField': 'studentId', 
+                    'foreignField': '_id', 
+                    'as': 'student'
+                }
+            }, {
+                '$lookup': {
+                    'from': 'discipline', 
+                    'localField': 'discipline', 
+                    'foreignField': '_id', 
+                    'as': 'discipline'
+                }
+            }, {
+                '$unwind': {
+                    'path': '$discipline', 
+                    'preserveNullAndEmptyArrays': false
+                }
+            }, {
+                '$unwind': {
+                    'path': '$teacher', 
+                    'preserveNullAndEmptyArrays': false
+                }
+            }, {
+                '$unwind': {
+                    'path': '$student', 
+                    'preserveNullAndEmptyArrays': false
+                }
+            }, {
+                '$addFields': {
+                    'discipline': '$discipline.name', 
+                    'name': {
+                        '$concat': [
+                            `$${reverseKey}.name`, ' - ', '$discipline.name'
+                        ]
+                    }, 
+                    'id': {
+                        '$toString': '$_id'
+                    }
+                }
+            }, {
+                '$project': {
+                    'discipline': 0, 
+                    'teacher': 0,
+                    'student': 0
+                }
+            },
+            ...(restrictionIdKey?  [{
+                '$match': {
+                    [restrictionIdKey]: ids[restrictionIdKey]
+                }
+            }] : [])
+        ])
+
+        return response
     }
 
     async deleteStudentClasses({ teacherId, studentId, idList }, session) {
@@ -33,7 +122,7 @@ class ClassRepository extends BaseRepository {
      */
     async checkIfTimeConflictsWithOtherClasses({ teacherId, time, daysOfWeek }) {
         if (typeof teacherId === "string") 
-            teacherId = mongoose.Types.ObjectId(teacherId);
+            teacherId = Types.ObjectId(teacherId);
 
         const result = await this.$listAggregate([
             {
@@ -64,9 +153,9 @@ class ClassRepository extends BaseRepository {
      */
     async checkIfConflictsWithOtherClassesWithDiferentDiscipline({ teacherId, time, daysOfWeek, discipline }) {
         if (typeof teacherId === "string") 
-            teacherId = mongoose.Types.ObjectId(teacherId);
+            teacherId = Types.ObjectId(teacherId);
         if (typeof discipline === "string") 
-            discipline = mongoose.Types.ObjectId(discipline);
+            discipline = Types.ObjectId(discipline);
 
         const result = await this.$listAggregate([
             {
@@ -97,9 +186,9 @@ class ClassRepository extends BaseRepository {
      */
     async checkIfClassExists({ teacherId, time, daysOfWeek, studentId }) {
         if (typeof teacherId === "string") 
-            teacherId = mongoose.Types.ObjectId(teacherId);
+            teacherId = Types.ObjectId(teacherId);
         if (typeof studentId === "string") 
-            studentId = mongoose.Types.ObjectId(studentId);
+            studentId = Types.ObjectId(studentId);
 
         const result = await this.$listAggregate([
             {
@@ -122,4 +211,18 @@ class ClassRepository extends BaseRepository {
     }
 }
 
+/**
+ * @typedef {Object} ClassList
+ * @property {string} id - O id do processo
+ * @property {boolean} active - se ativo ou não
+ * @property {number} daysOfWeek - Inteiro representando o dia de semana
+ * @property {string} studentId - Id do estudante 
+ * @property {string} teacherId - Id do professor 
+ * @property {number} startTime - Tempo, em minutos, de início da aula 
+ * @property {number} endTime - Tempo, em minutos, do final da aula 
+ * @property {string} discipline - Nome da disciplina
+ * @property {string} studentName - Nome do estudante
+ * @property {Date} creationDate - Data de criação do processStep
+ * @property {Date} lastUpdateDate - Data de última atualização do processStep
+ */
 module.exports = ClassRepository
--- src/business/repositories/mongo/TeacherRepository.js
+++ src/business/repositories/mongo/TeacherRepository.js
@@ -6,117 +6,15 @@ class TeacherRepository extends BaseRepository {
     constructor() {
         super(TeacherModel)
     }
-
-    /**
-     * 
-     * @param { object } params
-     * @param { string } params.teacherId - Id do professor
-     * @param { string } [params.studentId = undefined] - Id do aluno 
-     *
-     * @returns { Promise<Array<ClassList>> }  O processo
-     *  
-     */
-    async listTeacherClasses({ teacherId, studentId = undefined }, session ) {
-        if (typeof teacherId === "string")
-            teacherId = Types.ObjectId(teacherId)
-        if (studentId && typeof studentId === "string")
-            studentId = Types.ObjectId(studentId)
-
-        const classes = await this.$listAggregate([
-            {
-                '$match': {
-                    '_id': teacherId
-                }
-            }, {
-                '$lookup': {
-                    'from': 'class', 
-                    'localField': '_id', 
-                    'foreignField': 'teacherId', 
-                    'as': 'classes'
-                }
-            }, {
-                '$unwind': {
-                    'path': '$classes', 
-                    'preserveNullAndEmptyArrays': false
-                }
-            }, {
-                '$replaceRoot': {
-                    'newRoot': '$classes'
-                }
-            }, {
-                '$unwind': {
-                    'path': '$daysOfWeek', 
-                    'preserveNullAndEmptyArrays': false
-                }
-            }, {
-                '$lookup': {
-                    'from': 'student', 
-                    'localField': 'studentId', 
-                    'foreignField': '_id', 
-                    'as': 'student'
-                }
-            }, {
-                '$lookup': {
-                    'from': 'discipline', 
-                    'localField': 'discipline', 
-                    'foreignField': '_id', 
-                    'as': 'discipline'
-                }
-            }, {
-                '$unwind': {
-                    'path': '$discipline', 
-                    'preserveNullAndEmptyArrays': false
-                }
-            }, {
-                '$unwind': {
-                    'path': '$student', 
-                    'preserveNullAndEmptyArrays': false
-                }
-            }, {
-                '$addFields': {
-                    'discipline': '$discipline.name', 
-                    'name': {
-                        '$concat': [
-                            '$student.name', ' - ', '$discipline.name'
-                        ]
-                    },
-                    'id': {
-                        '$toString':'$_id'
-                    }
-                }
-            }, {
-                '$project': {
-                    'student': 0
-                }
-            },
-            ...(studentId?  [{
-                '$match': {
-                    'studentId': studentId
-                }
-            }] : [])
-        ], session)
-
-        let result = false
-
-        if (classes.length)
-            result = classes
-
-        return result;
-    }
 }
 
 /**
- * @typedef {Object} ClassList
- * @property {string} id - O id do processo
- * @property {boolean} active - se ativo ou não
- * @property {number} daysOfWeek - Inteiro representando o dia de semana
- * @property {string} studentId - Id do estudante 
- * @property {string} teacherId - Id do professor 
- * @property {number} startTime - Tempo, em minutos, de início da aula 
- * @property {number} endTime - Tempo, em minutos, do final da aula 
- * @property {string} discipline - Nome da disciplina
- * @property {string} studentName - Nome do estudante
- * @property {Date} creationDate - Data de criação do processStep
- * @property {Date} lastUpdateDate - Data de última atualização do processStep
+ * @typedef { object } Teacher
+ * @property { string } name - Nome do professor
+ * @property { string } email - Email do professor
+ * @property { string } password - Senha hasheada do professor
+ * @property { boolean } active - Se está ativo ou não
+ * @property { Date } creationDate - Data de criação do document
+ * @property { Date } lastUpdateDate - Data de última atualização do document
  */
 module.exports = TeacherRepository
