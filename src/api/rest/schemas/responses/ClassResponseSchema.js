0706548e45d36dcdef2e39ea9c7e3c7959cc992b
Elias Bernardo <ebmm01@gmail.com>
2021-01-29 19:18:04 -0300

    Adding ClassResponseSchema

---
+++ src/api/rest/schemas/responses/ClassResponseSchema.js
@@ -0,0 +1,12 @@
+module.exports = {
+    id: { type: 'string' },
+    name: { type: 'string' },
+    daysOfWeek: { type: 'number' },
+    active: { type: 'boolean' },
+    studentId: { type: 'string' },
+    teacherId: { type: 'string' },
+    startTime: { type: 'number' },
+    endTime: { type: 'number' },
+    lastUpdateDate: { type: 'string' },
+    creationDate: { type: 'string' }
+}
