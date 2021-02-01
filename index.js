83197af70d0ee6ed471c54589f33b24c1eeca84b
Elias Bernardo <ebmm01@gmail.com>
2021-01-31 22:52:28 -0300

    Adding dbs init to index and workerPool with zeroMq strategy

--- index.js
+++ index.js
@@ -4,11 +4,26 @@ const startServer = require("./src");
 const config = require('./src/config/config');
 const { hashHandlerInstance } = require('./src/business/lib/auth/HashHandler');
 const { authHandlerInstance } = require('./src/business/lib/auth/AuthHandler');
+const { dbInstance } = require('./src/db/index');
+const { redisDbInstance } = require('./src/db/redis');
+const { workerPoolInstance, workerTypes } = require('./src/business/lib/workers/WorkerPool');
+const ZeroMqStrategy = require('./src/business/lib/workers/zeroMq/ZeroMqStrategy');
+const WebsocketHandlerWorkerService = require('./src/workers/WebsocketHandlerWorkerService');
 
 
 (async () => {
+    await dbInstance.connect(config.db);
+
+    await redisDbInstance
+        .config(config.redis)
+        .connect({ prefix: "professoraJane" });
+
     hashHandlerInstance.configureHandler(config.hash)
     authHandlerInstance.configureHandler(config.auth)
+
+    workerPoolInstance
+        .addStrategy(ZeroMqStrategy, 'zeroMq', config.workerStrategies.zeroMq)
+        .addWorker(new WebsocketHandlerWorkerService(), { strategyName: "zeroMq", workerType: workerTypes.pubsub });
 
     await startServer()
 })()
