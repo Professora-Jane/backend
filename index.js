51dd49aaf27c8e17f2708c90e79443dc7445e1e7
Elias Bernardo <ebmm01@gmail.com>
2021-02-04 20:31:14 -0300

    Updating workerPool configuration

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { authHandlerInstance } = require('./src/business/lib/auth/AuthHandler');
 const { dbInstance } = require('./src/db/index');
 const { redisDbInstance } = require('./src/db/redis');
 const { workerPoolInstance, workerTypes } = require('./src/business/lib/workers/WorkerPool');
-const ZeroMqStrategy = require('./src/business/lib/workers/zeroMq/ZeroMqStrategy');
+const ZeroMqStrategy = require('./src/business/lib/workers/strategies/ZeroMqStrategy');
 const WebsocketHandlerWorkerService = require('./src/workers/WebsocketHandlerWorkerService');
 
 
@@ -21,9 +21,10 @@ const WebsocketHandlerWorkerService = require('./src/workers/WebsocketHandlerWor
     hashHandlerInstance.configureHandler(config.hash)
     authHandlerInstance.configureHandler(config.auth)
 
-    workerPoolInstance
+    await workerPoolInstance
         .addStrategy(ZeroMqStrategy, 'zeroMq', config.workerStrategies.zeroMq)
-        .addWorker(new WebsocketHandlerWorkerService(), { strategyName: "zeroMq", workerType: workerTypes.pubsub });
+        .addWorker(new WebsocketHandlerWorkerService(), { strategyName: 'zeroMq', workerType: workerTypes.pubsub })
+        .init()
 
     await startServer()
 })()
